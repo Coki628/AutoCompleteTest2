118b48d985a406eceaf3354eabd5c7343aff9e79
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2017-11-12 16:56:14 +0900

    20171112 ・外部連携機能を実装

--- app/src/main/java/com/example/srcn4/autocompletetest2/MapsActivity.java
+++ app/src/main/java/com/example/srcn4/autocompletetest2/MapsActivity.java
@@ -3,9 +3,13 @@ package com.example.srcn4.autocompletetest2;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,7 +26,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMarkerDragListener {
 
     private ArrayList<StationVO> stationList;
+    private StationVO resultStation;
     private LatLng centerLatLng;
+    private LatLng resultStationLatLng;
     private ArrayList<Double> latList = new ArrayList<>();
     private ArrayList<Double> lngList = new ArrayList<>();
     private ArrayList<LatLng> latLngList = new ArrayList<>();
@@ -35,12 +41,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // MainActivityから駅情報リストを受け取る
         Intent intent = getIntent();
         stationList =
-                (ArrayList<StationVO>)intent.getSerializableExtra("result");
+                (ArrayList<StationVO>)intent.getSerializableExtra("stationList");
+        resultStation = (StationVO)intent.getSerializableExtra("resultStation");
 
+        // 緯度経度を計算用のリストに格納
         for (StationVO vo : stationList) {
-            // 受け取った駅情報の確認
-            Log.d("map", vo.toString());
-            // 緯度経度を計算用のリストに格納
             latList.add(Double.parseDouble(vo.getLat()));
             lngList.add(Double.parseDouble(vo.getLng()));
         }
@@ -63,6 +68,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         // 中間地点座標の取得
         centerLatLng = LatLngCalculator.calcCenterLatLng(latList, lngList);
+        // 候補駅の座標を取得
+        resultStationLatLng = new LatLng(Double.parseDouble(resultStation.getLat()),
+                Double.parseDouble(resultStation.getLng()));
         // 最大距離の取得
         double[] maxDistance = LatLngCalculator.calcMaxDistance(latList, lngList);
         double maxDistanceLat = maxDistance[0];
@@ -85,12 +93,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         } else {
             zoomLevel = 9;
         }
-        // 中間地点に色違いのピンをセットして情報ウインドウも表示
+        // 候補駅に色違いのピンをセットして情報ウインドウも設定する
+        Marker stationMarker = map.addMarker(new MarkerOptions().position(resultStationLatLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .title(resultStation.getName() + "駅！"));
+        // 情報ウインドウを表示
+        stationMarker.showInfoWindow();
+        // 中間地点に色違いのピンをセットして情報ウインドウも設定する
         Marker centerMarker = map.addMarker(new MarkerOptions().position(centerLatLng)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
                 .draggable(true)
                 .title("中間地点！"));
-        centerMarker.showInfoWindow();
+        // フォーカスを当てるマーカーを設定
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, zoomLevel));
         // 情報ウインドウのリスナーをセット
         map.setOnInfoWindowClickListener(this);
@@ -131,5 +145,108 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
                 })
                 .show();
+    }
+
+    // 共有ボタンが押された時
+    public void callLINE(View v) {
+
+        // LINEのアプリID
+        final String LINE_APP_ID = "jp.naver.line.android";
+        // LINEで送る用の改行コード
+        final String LINE_SEPARATOR = "%0D%0A";
+
+        try {
+            // パッケージ情報の取得
+            PackageManager pm = getPackageManager();
+            // LINEがインストールされているかの確認
+            ApplicationInfo appInfo = pm.getApplicationInfo(LINE_APP_ID, PackageManager.GET_META_DATA);
+            // インストールされてたら、LINEへ
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("line://msg/text/" + "中間地点は…" + LINE_SEPARATOR
+                    + resultStation.getName() + "駅" + LINE_SEPARATOR
+                    + "だよ！" + LINE_SEPARATOR
+                    + "from 中間地点アプリ"
+            ));
+            startActivity(intent);
+
+        } catch(PackageManager.NameNotFoundException e) {
+            //インストールされてなかったら、インストールを要求する
+            new AlertDialog.Builder(this)
+                    .setTitle("LINEが見つかりません。")
+                    .setMessage("LINEをインストールしてやり直して下さい。")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // 特に何もしない
+                        }
+                    })
+                    .setCancelable(false)
+                    .show();
+        }
+    }
+
+    // ジャンルボタンが押された時
+    public void callGenre(View v) {
+
+        final String[] items = {"レストラン", "居酒屋", "カフェ", "コンビニ", "カラオケ"};
+        int defaultItem = 0; // デフォルトでチェックされているアイテム
+        final ArrayList<Integer> checkedItems = new ArrayList<>();
+        checkedItems.add(defaultItem);
+        new AlertDialog.Builder(this)
+                .setTitle("ジャンル選択")
+                .setSingleChoiceItems(items, defaultItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        checkedItems.clear();
+                        checkedItems.add(which);
+                    }
+                })
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!checkedItems.isEmpty()) {
+                            Log.d("checkedItem:", "" + checkedItems.get(0));
+                            // ジャンルが決定されたら次の画面へ遷移
+                            callBrowser(checkedItems.get(0));
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    // ブラウザ呼び出し用メソッド
+    private void callBrowser(int genre) {
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        // ジャンル別で接続先を分ける
+        switch (genre){
+            // レストラン系はぐるナビ
+            case 0:
+                intent.setData(Uri.parse("https://r.gnavi.co.jp/eki/"
+                        + resultStation.getGnaviId() + "/rs/"));
+                break;
+            case 1:
+                intent.setData(Uri.parse("https://r.gnavi.co.jp/eki/"
+                        + resultStation.getGnaviId() + "/izakaya/rs/"));
+                break;
+            case 2:
+                intent.setData(Uri.parse("https://r.gnavi.co.jp/eki/"
+                        + resultStation.getGnaviId() + "/cafe/rs/"));
+                break;
+            // コンビニとカラオケはグーグルマップ
+            case 3:
+                intent.setData(Uri.parse("https://www.google.co.jp/maps/search/コンビニ/@"
+                        + resultStation.getLat() + "," + resultStation.getLng() + "," + "15z")); // 15zはズーム具合
+                break;
+            case 4:
+                intent.setData(Uri.parse("https://www.google.co.jp/maps/search/カラオケ/@"
+                        + resultStation.getLat() + "," + resultStation.getLng() + "," + "15z"));
+                break;
+        }
+        // 上記URLでブラウザを起動する
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/srcn4/autocompletetest2/ResultActivity.java
+++ app/src/main/java/com/example/srcn4/autocompletetest2/ResultActivity.java
@@ -1,6 +1,11 @@
 package com.example.srcn4.autocompletetest2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,10 +19,7 @@ import java.util.ArrayList;
 public class ResultActivity extends AppCompatActivity {
 
     private ArrayList<StationVO> stationList;
-    private LatLng centerLatLng;
-    private ArrayList<Double> latList = new ArrayList<>();
-    private ArrayList<Double> lngList = new ArrayList<>();
-    private TextView searchResult;
+    private StationVO resultVO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +31,70 @@ public class ResultActivity extends AppCompatActivity {
         stationList =
                 (ArrayList<StationVO>) intent.getSerializableExtra("result");
 
+        // 計算用の緯度と経度のリスト
+        ArrayList<Double> latList = new ArrayList<>();
+        ArrayList<Double> lngList = new ArrayList<>();
+        // 緯度経度を駅情報リストから計算用のリストに格納
         for (StationVO vo : stationList) {
-            // 受け取った駅情報の確認
-            Log.d("map", vo.toString());
-            // 緯度経度を計算用のリストに格納
+
             latList.add(Double.parseDouble(vo.getLat()));
             lngList.add(Double.parseDouble(vo.getLng()));
         }
         // 中間地点座標の取得
-        centerLatLng = LatLngCalculator.calcCenterLatLng(latList, lngList);
-        // 最寄り駅を調べる
-        StationVO resultVO = LatLngCalculator.calcNearestStation(centerLatLng, getApplicationContext());
+        LatLng centerLatLng = LatLngCalculator.calcCenterLatLng(latList, lngList);
+        // 中間地点から一番近い座標にある駅を調べる
+        resultVO = LatLngCalculator.calcNearestStation(centerLatLng, getApplicationContext());
 
-        searchResult = findViewById(R.id.search_result);
+        TextView searchResult = findViewById(R.id.search_result);
         searchResult.setText(resultVO.getName());
     }
 
     // 共有ボタンが押された時
     public void callLINE(View v) {
 
+        // LINEのアプリID
+        final String LINE_APP_ID = "jp.naver.line.android";
+        // LINEで送る用の改行コード
+        final String LINE_SEPARATOR = "%0D%0A";
+
+        try {
+            // パッケージ情報の取得
+            PackageManager pm = getPackageManager();
+            // LINEがインストールされているかの確認
+            ApplicationInfo appInfo = pm.getApplicationInfo(LINE_APP_ID, PackageManager.GET_META_DATA);
+            // インストールされてたら、LINEへ
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("line://msg/text/" + "中間地点は…" + LINE_SEPARATOR
+                    + resultVO.getName() + "駅" + LINE_SEPARATOR
+                    + "だよ！" + LINE_SEPARATOR
+                    + "from 中間地点アプリ"
+            ));
+            startActivity(intent);
+
+        } catch(PackageManager.NameNotFoundException e) {
+            //インストールされてなかったら、インストールを要求する
+            new AlertDialog.Builder(this)
+                    .setTitle("LINEが見つかりません。")
+                    .setMessage("LINEをインストールしてやり直して下さい。")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // 特に何もしない
+                        }
+                    })
+                    .setCancelable(false)
+                    .show();
+        }
+    }
+
+    // 周辺情報ボタンが押された時
+    public void callMapInfo(View v) {
+
+        // 画面遷移処理で、駅情報のリストを次の画面に送る
+        Intent intent = new Intent(ResultActivity.this, MapsActivity.class)
+                .putExtra("stationList", stationList)
+                .putExtra("resultStation", resultVO);
+        startActivity(intent);
     }
 }
