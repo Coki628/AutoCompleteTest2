5c27df18e09e98fd3143c65183056e93be5c6a55
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2017-11-23 03:49:21 +0900

    20171122 ・画面を縦固定 ・ルート画面の表示など(まだ途中)

--- app/src/main/java/com/example/srcn4/autocompletetest2/fragments/PageFragment.java
+++ app/src/main/java/com/example/srcn4/autocompletetest2/fragments/PageFragment.java
@@ -11,9 +11,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.srcn4.autocompletetest2.R;
-import com.example.srcn4.autocompletetest2.models.StationDetailVO;
 import com.example.srcn4.autocompletetest2.models.StationTransferVO;
-import com.example.srcn4.autocompletetest2.models.StationVO;
 
 import java.util.ArrayList;
 
@@ -46,18 +44,38 @@ public class PageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // 必要な値の受け取り
         int page = getArguments().getInt("page", 0) + 1;
         ArrayList<StationTransferVO>[] resultInfoLists =
                 (ArrayList<StationTransferVO>[])getArguments().getSerializable("resultInfoLists");
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_page, container, false);
         // 各タブに表示する内容を記述
         TextView textView = view.findViewById(R.id.textView);
         textView.setText("Page" + page);
-        // とりあえずログで必要な値がちゃんと送られてきてる確認
-        Log.d("test", String.valueOf(page));
+
+        // ソート処理ここに入れる
+
         for (int i = 0; i < resultInfoLists.length; i++) {
             Log.d("test", resultInfoLists[i].get(0).toString());
+            // route_info1～5に経路の情報を格納していく
+            TextView route_title =
+                    view.findViewById(getResources().getIdentifier(
+                    "route_info" + String.valueOf(i+1), "id", getActivity().getPackageName()))
+                    .findViewById(R.id.route_title);
+            route_title.setText((resultInfoLists[i].get(0).getName() + "　～　" + resultInfoLists[i].get(0).getDestName()));
+            TextView time = view.findViewById(getResources().getIdentifier(
+                    "route_info" + String.valueOf(i+1), "id", getActivity().getPackageName()))
+                    .findViewById(R.id.time);
+            time.setText(("所要時間" + resultInfoLists[i].get(0).getTime() + "分"));
+            TextView cost = view.findViewById(getResources().getIdentifier(
+                    "route_info" + String.valueOf(i+1), "id", getActivity().getPackageName()))
+                    .findViewById(R.id.cost);
+            cost.setText(("料金" + resultInfoLists[i].get(0).getCost() + "円"));
+            TextView transfer = view.findViewById(getResources().getIdentifier(
+                    "route_info" + String.valueOf(i+1), "id", getActivity().getPackageName()))
+                    .findViewById(R.id.transfer);
+            transfer.setText(("乗り換え" + resultInfoLists[i].get(0).getTransfer() + "回"));
         }
         return view;
     }
